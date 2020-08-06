1806fbaaee36e8c222f814f3a9d5ba897517f206
Junyi Ji <junyiji@gmail.com>
2020-08-06 15:48:10 +0200

    Initial commit

---
+++ docs/precache-manifest.88078e4eb613f9a7ad12a12b44c4bd91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24433e83dce2de9abf0045b8446e4f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8eabf298cff3b2447658",
+    "url": "./static/css/main.bb7fcec3.chunk.css"
+  },
+  {
+    "revision": "12f62c2fe875b5d30aa8",
+    "url": "./static/js/2.d006b8c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d006b8c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eabf298cff3b2447658",
+    "url": "./static/js/main.463d1f74.chunk.js"
+  },
+  {
+    "revision": "855dd37ae80842983b98",
+    "url": "./static/js/runtime-main.82c6c62f.js"
+  }
+]);
