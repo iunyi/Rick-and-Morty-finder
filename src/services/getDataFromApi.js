449888931eeb9775636b62da56e21d21c5c2fb57
Junyi Ji <junyiji@gmail.com>
2020-12-29 00:05:16 +0100

    Fetch multiple pages

--- src/services/getDataFromApi.js
+++ src/services/getDataFromApi.js
@@ -1,9 +1,19 @@
-function getDataFromApi(){
-    return fetch('https://rickandmortyapi.com/api/character/')
-        .then(response => response.json())
-        .then(responseData => {
-            return responseData.results
-        })
-}
+const getDataFromApi = async () => {
+    const fetchedData = await Promise.all([
+        fetchData('https://rickandmortyapi.com/api/character/'),
+        fetchData('https://rickandmortyapi.com/api/character/?page=2'),
+        fetchData('https://rickandmortyapi.com/api/character/?page=3'),
+        fetchData('https://rickandmortyapi.com/api/character/?page=4'),
+        fetchData('https://rickandmortyapi.com/api/character/?page=5'),
+        fetchData('https://rickandmortyapi.com/api/character/?page=10'),
+    ]);
+    return fetchedData.flat();
+};
+
+const fetchData = async (endpoint) => {
+    const response = await fetch(endpoint);
+    const responseData = await response.json();
+    return responseData.results;
+};
 
 export default getDataFromApi;
